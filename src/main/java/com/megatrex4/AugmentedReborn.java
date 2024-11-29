910a0f215044b66d8a1c5f6981aec745163b6ea1
Eugene <skyzgamer@outlook.com>
2024-11-29 07:40:14 +0200

    fix tooltips

--- src/main/java/com/megatrex4/AugmentedReborn.java
+++ src/main/java/com/megatrex4/AugmentedReborn.java
@@ -26,10 +26,6 @@ public class AugmentedReborn implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static ScreenHandlerType<CustomGrindstoneScreenHandler> CUSTOM_GRINDSTONE_HANDLER;
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-
 	@Override
 	public void onInitialize() {
 		ItemRegistry.ItemRegistry();
--- src/main/java/com/megatrex4/event/TooltipEventListener.java
+++ src/main/java/com/megatrex4/event/TooltipEventListener.java
@@ -4,6 +4,8 @@ import com.megatrex4.AugmentedReborn;
 import com.megatrex4.items.ActivatableItem;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
@@ -23,7 +25,7 @@ public class TooltipEventListener {
 
     private static void addTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip) {
         // Check if the item is an instance of ActivatableItem or its specific class
-        if (stack.getItem() instanceof ActivatableItem activatable) {
+        if (stack.getItem() instanceof ActivatableItem activatable && isHelmet(stack)) {
             boolean isActivated = activatable.isActivated(stack);
             tooltip.add(Text.translatable("item.augmented_reborn.night_vision.status")
                     .append(" ")
@@ -35,5 +37,10 @@ public class TooltipEventListener {
                     .append(Text.translatable("item.augmented_reborn.night_vision.disabled")
                             .formatted(Formatting.RED))); // Default status
         }
+    }
+
+    private static boolean isHelmet(ItemStack stack) {
+        // Check if the item is an ArmorItem and is for the head slot
+        return stack.getItem() instanceof ArmorItem armorItem && armorItem.getSlotType() == EquipmentSlot.HEAD;
     }
 }
